d944cd577edfbfd6e6081541a3358cea237f4dba
Felipe Luiz <felipe.andrade33@etec.sp.gov.br>
2021-11-23 15:04:54 -0300

    estruturado e estilizada parte estatica sem responsividade tela de exibição dos filmes

--- src/app/movies-screen/movies-screen.component.ts
+++ src/app/movies-screen/movies-screen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-movies-screen',
   templateUrl: './movies-screen.component.html',
--- src/app/movies-screen/movies-screen.module.ts
+++ src/app/movies-screen/movies-screen.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesScreenComponent } from './movies-screen.component';
-
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { MoviesScreenComponent } from './movies-screen.component';
     MoviesScreenComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [
     MoviesScreenComponent
