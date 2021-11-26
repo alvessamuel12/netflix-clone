6351c29a9aa0861001a0cba19d5d985fb0f61ffd
Samuel Alves <alvessamuel12@outlook.com>
2021-11-26 00:14:18 -0300

    Adicionando rotas e algumas modificações na tela de users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login/login.component'
+import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { MoviesScreenComponent } from './movies-screen/movies-screen.component';
 
 const routes: Routes = [
-  {path: "", component: LoginComponent}
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "profiles", component: UsersComponent},
+  {path: "browse", component: MoviesScreenComponent}
 ];
 
 @NgModule({
